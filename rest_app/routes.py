3e951fa62edb2a0a742051727cba9b7e1bddadf4
dudakp <palodudak@gmail.com>
2021-07-12 20:49:09 +0200

    logging

--- rest_app/routes.py
+++ rest_app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify, Response
+import json
+
+from flask import Blueprint, jsonify, Response, request
 
 from common.decorators import inject_query_params
 from common.utils.logging_utils import get_logger
@@ -7,11 +9,12 @@ rest_app = Blueprint('rest_app', __name__)
 
 logger = get_logger(__name__)
 
-
+# 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
 @rest_app.post('/rate')
 @inject_query_params(default_values={
     'payload': {'default_value': '', 'type': str}
 })
 def create_module_rating(payload: str, **kwargs) -> Response:
+    logger.info(request.data)
     logger.info(f'got payload:{payload}')
-    return jsonify({'data': payload})
+    return jsonify({'data': json.loads(request.data)})
